117299f354565695b2eeab4b46036f9e7424ef3d
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-05-10 12:21:27 +0530

    push 5

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -11,7 +11,7 @@ const Tour = ({ id, name, info, image, price, removeTour }) => {
         {readMore ? info.slice(0, 200) + "..." : info}
         <br />
         <button id={`see-more-${id}`} onClick={() => setReadMore(!readMore)}>
-          {readMore ? "See more" : "See less"}
+          {readMore ? "See more" : "Show less"}
         </button>
 
         <br />
